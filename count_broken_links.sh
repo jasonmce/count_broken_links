0de9f7a1795dcb6b7b2cdb97d051e09712f6e1ef
McEachen, Jason <mceachenj@hhmi.org>
2016-03-01 10:23:51 -0500

    Adds file cleanup to startup. Now uses the --debug flag to quickly match source with broken links.

--- count_broken_links.sh
+++ count_broken_links.sh
@@ -62,15 +62,23 @@ esac
 shift # past argument or value
 done
 
+
 filename="/tmp/count_broken_links-"$HOSTNAME
 
-wget --spider -r -nd -nv $WAIT $REJLIST $ACCLIST $LEVEL $IMAGES --header='User-Agent: Mozilla/5.0' -o $filename http://$HOSTNAME/$URLPATH
+# Possibly do a less destructive cleanup later, but for now...
+rm ${filename}
+rm ${filename}.short
+
+wget --spider --recursive --no-directories --no-verbose --debug --reject-regex "\?" $WAIT $REJLIST $ACCLIST $LEVEL $IMAGES -o ${filename} --header='User-Agent: Mozilla/5.0' http://$HOSTNAME/$URLPATH
 
 grep "Found no broken links." $filename
 if [ $? -eq 0 ] ; then
   echo "No broken links found"
   exit $STATE_OK
 fi
+
+# Save a short list of referrer -> broken link pairs.
+cat $filename | sed -n "/Referer\|^http\|broken/p" | sed -n -e "/broken/{x;p;d}; x" > ${filename}.short
 
 count=$(grep -P '\d+ (?=broken)' -o $filename)
 
